53292cd1028dd87e056aa9637d193d08da0ec262
sjmoon <tmdwn752@naver.com>
2023-06-12 23:00:44 +0900

    결제기능구현_230612

--- src/main/resources/static/js/cart/cart.js
+++ src/main/resources/static/js/cart/cart.js
@@ -29,5 +29,42 @@ function decreaseQuantity(element){
     const totalText = new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(totalPrice);
     $("#totalSubText").text(totalText);
     $("#totalText").text(totalText);
-
 }
+
+// input 리스트를 담을 배열 생성
+        var inputList = [];
+
+        // tr 요소들을 순회하며 원하는 name 속성을 가진 input 값을 배열에 추가
+        $("tr input[name='cartTr']").each(function() {
+            var cartSeq = $(this).val();
+            var productName = $("#productName" + cartSeq).val();
+            var size = $("#size" + cartSeq).val();
+            var price = $("#price" + cartSeq).val();
+
+            // 값들을 객체로 생성하여 배열에 추가
+            var input = {
+                cartSeq: cartSeq,
+                productName: productName,
+                size: size,
+                price: price
+            };
+
+            inputList.push(input);
+        });
+
+        // 컨트롤러로 inputList를 전달
+        $.ajax({
+            type: "POST",
+            url: "/controller-url",
+            data: JSON.stringify(inputList),
+            contentType: "application/json",
+            success: function(response) {
+                // 성공적으로 처리되었을 때의 동작
+                console.log(response);
+            },
+            error: function() {
+                // 오류 발생 시의 동작
+                console.log("오류 발생");
+            }
+        });
+    }
