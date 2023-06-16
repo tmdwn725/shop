4ddaab57f43a6a9725c5fd1460b1797ff52929ac
sjmoon <tmdwn752@naver.com>
2023-06-16 21:28:04 +0900

    결제기능구현_230616

--- src/main/java/com/shop/controller/PaymentController.java
+++ src/main/java/com/shop/controller/PaymentController.java
@@ -1,14 +1,22 @@
 package com.shop.controller;
 
+import com.shop.dto.CartDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class PaymentController {
-    @RequestMapping("/payment")
-    public String getPaymentInfo(){
+    @RequestMapping(value = "/payment", method = {RequestMethod.POST, RequestMethod.GET})
+    public String getPaymentInfo(Model model, @ModelAttribute("CartDTO") CartDTO cartDTO){
+        model.addAttribute("myCartList",cartDTO.getCartList());
         return "payment/payment";
     }
 }
--- src/main/java/com/shop/dto/CartDTO.java
+++ src/main/java/com/shop/dto/CartDTO.java
@@ -2,12 +2,19 @@ package com.shop.dto;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class CartDTO {
+    public List<CartDTO> cartList= new ArrayList();
     private Long cartSeq;
     private MemberDTO member;
     private ProductStockDTO productStock;
     private int quantity;
     private Long memberSeq;
     private Long productStockSeq;
+    private String productName;
+    private String productSize;
+    private int price;
 }
